9a83f7cb8375ec37d1659e4530ee56dea1c5ea32
misaacs21 <mail.m.isaacs@gmail.com>
2022-10-30 21:05:20 -0400

    Some kind of reactivity

--- src/interface.js
+++ src/interface.js
@@ -1,5 +1,29 @@
+const component = {
+    template: null,
+    keys: null,
+    root: null
+}
+
+export const reactive = (data) => {
+    const reactiveData = {}
+    for (let [key, value] of Object.entries(data)) {
+        reactiveData[key] = value
+        Object.defineProperty(data, key, {
+            get() { return reactiveData[key] },
+            set(val) {
+                reactiveData[key] = val
+                const template = compile(this)(component.template, ...component.keys)
+                render(template, component.root)
+            }
+        })
+    }
+    return data
+}
+
 export const compile = (data) => {
     return (template, ...keys) => {
+        component.template = template
+        component.keys = keys
         let compiled = template[0]
         for (let i = 0; i < keys.length; i++) {
             compiled += data[keys[i]]
@@ -9,6 +33,7 @@ export const compile = (data) => {
     }
 }
 export const render = (template, root = document.body) => {
+    component.root = root
     root.innerHTML = template
 }
 
--- test/mount.test.js
+++ test/mount.test.js
@@ -1,6 +1,15 @@
 import { describe, expect, it } from 'vitest'
-import { compile,  render } from '@/interface.js'
+import { compile,  render, reactive } from '@/interface.js'
 
+describe('reactive', () => {
+    it('recompiles and rerenders on data change', () => {
+        const data = reactive({ string: 'world' })
+        const template = compile(data)`<div>Hello ${ 'string' }</div>`
+        render(template)
+        data.string = 'Dave'
+        expect(document.body.children[0].textContent).toBe('Hello Dave')
+    })
+})
 describe('compile', () => {
     it('interpolates string with data', () => {
         const data = { string: 'world' }
