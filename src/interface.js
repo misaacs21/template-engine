618485ffb2d69846707b35c44341074e8f3ad622
misaacs21 <mail.m.isaacs@gmail.com>
2022-11-16 21:48:02 -0500

    Change to DOM traversing and parsing for more efficient DOM updates + custom delimiters

--- src/interface.js
+++ src/interface.js
@@ -1,39 +1,82 @@
-const component = {
-    template: null,
-    keys: null,
-    root: null
+const parser = new DOMParser()
+let templateDelimiters
+let templateRegex
+
+class Subscriber {
+    constructor(domElement) {
+        this.domElement = domElement;
+        this.templateContent = domElement.textContent;
+    }
+}
+
+const replaceDelimiters = (data, text) => {
+    return text.replace(templateRegex, (match) => {
+        const key = match.substring(templateDelimiters[0].length, match.length - templateDelimiters[1].length).trim()
+        const content = data[key]
+        if (content !== undefined) {
+            return content.value
+        }
+        return match
+    })
+}
+
+const compileNodeChildren = (root, data) => {
+    if (root.childNodes.length === 0) {
+        return
+    }
+    for (let child of root.childNodes) {
+        if (child.nodeType === Node.TEXT_NODE) {
+            child.textContent = child.textContent.replace(templateRegex, (match) => {
+                const key = match.substring(templateDelimiters[0].length, match.length - templateDelimiters[1].length).trim()
+                const content = data[key]
+                if (content !== undefined) {
+                    content.subscribers.push(new Subscriber(child))
+                    return content.value
+                }
+                return match
+            })        
+        }
+        compileNodeChildren(child, data)
+    }      
 }
 
 export const reactive = (data) => {
     const reactiveData = {}
     for (let [key, value] of Object.entries(data)) {
-        reactiveData[key] = value
+        reactiveData[key] = {
+            value: value,
+            subscribers: []
+        }
         Object.defineProperty(data, key, {
-            get() { return reactiveData[key] },
+            subscribers: [],
+            get () { return reactiveData[key] },
             set(val) {
-                reactiveData[key] = val
-                const template = compile(this)(component.template, ...component.keys)
-                render(template, component.root)
+                reactiveData[key].value = val
+                for (let subscriber of reactiveData[key].subscribers) {
+                    subscriber.domElement.textContent = replaceDelimiters(this, subscriber.templateContent)
+                }
             }
         })
     }
     return data
 }
 
-export const compile = (data) => {
-    return (template, ...keys) => {
-        component.template = template
-        component.keys = keys
-        let compiled = template[0]
-        for (let i = 0; i < keys.length; i++) {
-            compiled += data[keys[i]]
-            compiled += template[i+1]
+export const compile = (data, delimiters = ['{{', '}}']) => {
+    templateDelimiters = delimiters
+    templateRegex = new RegExp(`${templateDelimiters[0]}.*?${templateDelimiters[1]}`, 'gs')
+    return (template) => {
+        const domTree = document.createDocumentFragment()
+        const html = parser.parseFromString(template[0], 'text/html').body.children
+        for (const child of html) {
+            domTree.appendChild(child)
         }
-        return compiled
+        for (const child of domTree.children) {
+            compileNodeChildren(child, data)
+        }
+        return domTree
     }
 }
 export const render = (template, root = document.body) => {
-    component.root = root
-    root.innerHTML = template
+    root.appendChild(template)
 }
 
--- test/mount.test.js
+++ test/mount.test.js
@@ -4,7 +4,7 @@ import { compile,  render, reactive } from '@/interface.js'
 describe('reactive', () => {
     it('recompiles and rerenders on data change', () => {
         const data = reactive({ string: 'world' })
-        const template = compile(data)`<div>Hello ${ 'string' }</div>`
+        const template = compile(data)`<div>Hello {{ string }}</div>`
         render(template)
         data.string = 'Dave'
         expect(document.body.children[0].textContent).toBe('Hello Dave')
@@ -12,16 +12,23 @@ describe('reactive', () => {
 })
 describe('compile', () => {
     it('interpolates string with data', () => {
-        const data = { string: 'world' }
-        const template = compile(data)`<div>Hello ${ 'string' }</div>`
-        expect(template).toBe(`<div>Hello world</div>`)
+        const data = reactive({ string: 'world' })
+        const template = compile(data)`<div>Hello {{ string }}</div>`
+        expect(template.textContent).toBe(`Hello world`)
+    })
+    it('interpolates nested elements with data', () => {
+        const data = reactive({ string: 'world', string2: 'hello' })
+        const template = compile(data)`<div>Hello {{ string }} <p>{{ string2 }} <span>{{ string }}!</span></p></div>`
+        expect(template.textContent).toBe(`Hello world hello world!`)
+    })
+    it('does not alter non-reactive elements', () => {
+        const data = reactive({ string: 'world' })
+        const template = compile(data)`<div>Hello {{ string }} <p>hello!</p></div>`
+        expect(template.textContent).toBe(`Hello world hello!`)
+    })
+    it('accepts different delimiters', () => {
+        const data = reactive({ string: 'world' })
+        const template = compile(data, ['<%=', '%>'])`<div>Hello <%= string %></div>`
+        expect(template.textContent).toBe(`Hello world`)
     })
 })
-
-describe('render', () => {
-    it('renders html to DOM', () => {
-        render(`<div>Hello world</div>`)
-        expect(document.body.children[0].textContent).toBe('Hello world')
-    })
-})
-
