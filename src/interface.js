1ab5c25f73278236d85ae952bcd417b73647a113
misaacs21 <mail.m.isaacs@gmail.com>
2023-02-22 21:17:27 -0500

    First pass fix event listeners, for loop

--- src/interface.js
+++ src/interface.js
@@ -15,50 +15,52 @@ class Subscriber {
     }
 }
 
-const replaceDelimiters = (data, text, subscriberCallback) => {
+const replaceDelimiters = ({ reactiveData, data }, text, subscriberCallback) => {
     return text.replace(templateRegex, (match) => {
         const value = match.substring(templateDelimiters[0].length, match.length - templateDelimiters[1].length).trim()
-        const content = data[value]
+        const content = reactiveData[value]
         if (content !== undefined) {
             if (subscriberCallback) subscriberCallback(content)
-            return content.value
+            return data[value]
         }
         
         const relevantData = Object.keys(data).filter((item) => {
             if (!value.includes(item)) return false
-            if (subscriberCallback) subscriberCallback(data[item])
+            if (subscriberCallback) subscriberCallback(reactiveData[item])
             return true
         })
         return relevantData.length > 0
-            ? Function(...relevantData, `return (${ value })`).bind(data)(...(relevantData.map((key) => data[key].value)))
+            ? Function(...relevantData, `return (${ value })`).bind(data)(...(relevantData.map((key) => data[key])))
             : Function(`return (${ value })`).bind(data)()
     })
 }
 
-const compileNode = (root, data) => {
+const compileNode = (root, { reactiveData, data }) => {
     if (root.nodeType === Node.TEXT_NODE) {
-        root.textContent = replaceDelimiters(data, root.textContent, (data) => data.subscribers.add(new Subscriber(root, 'text')))
+        root.textContent = replaceDelimiters({ reactiveData, data }, root.textContent, (data) => data.subscribers.add(new Subscriber(root, 'text')))
     }
     else if (root.nodeType === Node.ELEMENT_NODE) {
         const subscriber = new Subscriber(root, 'attr')
         for (const attribute of root.attributes) {
             switch(attribute.nodeName[0]) {
                 case '@':
-                    root.addEventListener(attribute.nodeName.substring(1), data[attribute.value].value.bind(data))
+                    root.addEventListener(attribute.nodeName.substring(1), data[attribute.value].bind(data))
                     break
                 case '&':
                     const loopName = attribute.value
                     root.removeAttribute(attribute.nodeName)
                     const loopNodes = []
-                    const loopData = data[attribute.nodeName.substring(attribute.nodeName.indexOf('-')+1)].value
+                    const loopData = data[attribute.nodeName.substring(attribute.nodeName.indexOf('-')+1)]
                     for (let i = 0; i < loopData.length; i++) {
-                        const newNode = compileNode(root.cloneNode(true), { ...data, [loopName]: { value: loopData[i], subscribers: new Set() }})
+                        const newData = reactive({ [loopName]: loopData[i] }, reactiveData)
+                        data[loopName] = newData[loopName]
+                        const newNode = compileNode(root.cloneNode(true), { reactiveData, data})
                         loopNodes.push(newNode)
                     }
                     root.replaceWith(...loopNodes)
                     break
                 default:
-                    root.setAttribute(attribute.nodeName, replaceDelimiters(data, attribute.value, (data) => {
+                    root.setAttribute(attribute.nodeName, replaceDelimiters({ reactiveData, data }, attribute.value, (data) => {
                         subscriber.addAttr(attribute.nodeName)
                         data.subscribers.add(subscriber)
                     }))
@@ -67,29 +69,29 @@ const compileNode = (root, data) => {
     }
 
     if (root.childNodes.length === 0) {
-        return
+        return root
     }
 
     for (const child of root.childNodes) {
-        compileNode(child, data)
+        compileNode(child, { reactiveData, data })
     }
+    return root
 }
 
-export const reactive = (data) => {
-    const reactiveData = {}
+const reactive = (data, reactiveData) => {
     for (const [key, value] of Object.entries(data)) {
         reactiveData[key] = {
             value: value,
             subscribers: new Set(),
         }
         Object.defineProperty(data, key, {
-            get () { return reactiveData[key] },
+            get () { return reactiveData[key].value },
             set(val) {
                 reactiveData[key].value = val
                 for (const subscriber of reactiveData[key].subscribers) {
                     switch (subscriber.updateType) {
                         case 'text':
-                            subscriber.domElement.textContent = replaceDelimiters(this, subscriber.templateContent)
+                            subscriber.domElement.textContent = replaceDelimiters({ reactiveData, data: this }, subscriber.templateContent)
                             break
                         case 'attr':
                             for (const attr of subscriber.attrs) {
@@ -105,20 +107,25 @@ export const reactive = (data) => {
 }
 
 export const compile = (data, delimiters = ['{{', '}}']) => {
+    const reactiveData = {}
+    reactive(data, reactiveData)
     templateDelimiters = delimiters
     templateRegex = new RegExp(`${templateDelimiters[0]}.*?${templateDelimiters[1]}`, 'gs')
     return (template) => {
         const domTree = document.createDocumentFragment()
-        const html = parser.parseFromString(template[0], 'text/html').body.children
+        const html = parser.parseFromString(template[0].trim(), 'text/html').body.children
 
         for (const child of Array.from(html)) {
             domTree.appendChild(child)
         }
 
         for (const child of domTree.children) {
-            compileNode(child, data)
+            compileNode(child, { reactiveData, data })
         }
-        return domTree
+        return {
+            template: domTree,
+            data: data
+        }
     }
 }
 export const render = (template, root = document.body) => {
--- test/mount.test.js
+++ test/mount.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it, beforeEach, vi } from 'vitest'
-import { compile,  render, reactive } from '@/interface.js'
+import { compile, render } from '@/interface.js'
 
 beforeEach(() => {
     document.body.innerHTML = ''
@@ -7,113 +7,97 @@ beforeEach(() => {
 
 describe('reactive render', () => {
     it('textContent updates on data change', () => {
-        const data = reactive({ string: 'world' })
-        const template = compile(data)`<div>Hello {{ string }}</div>`
+        const { template, data } = compile({ string: 'world' })`<div>Hello {{ string }}</div>`
         render(template)
         data.string = 'Dave'
         expect(document.body.textContent).toBe('Hello Dave')
     })
     it('attribute updates on data change', () => {
-        const data = reactive({ data: 'bar' })
-        const template = compile(data)`<div foo="{{ data }}">Hello world!</div>`
+        const { template, data } = compile({ data: 'bar' })`<div foo="{{ data }}">Hello world!</div>`
         render(template)
         data.data = 'foo'
         expect(document.body.querySelector('div').getAttribute('foo')).toBe('foo')
     })
     it('multiple attributes update on data change', () => {
-        const data = reactive({ data: 'bar' })
-        const template = compile(data)`<div foo="{{ data }}" bar="{{ data }}">Hello world!</div>`
+        const { template, data } = compile({ data: 'bar' })`<div foo="{{ data }}" bar="{{ data }}">Hello world!</div>`
         render(template)
         data.data = 'foo'
         expect(document.body.querySelector('div').getAttribute('foo')).toBe('foo')
         expect(document.body.querySelector('div').getAttribute('bar')).toBe('foo')
     })
     it('executed javascript updates on data change', () => {
-        const data = reactive({ num1: 1, num2: 2 })
-        const template = compile(data)`<div>My favorite number is {{ num1 + num2 }}</div>`
+        const { template, data} = compile({ num1: 1, num2: 2 })`<div>My favorite number is {{ num1 + num2 }}</div>`
         render(template)
         data.num1 = 2
         expect(document.body.textContent).toBe('My favorite number is 4')
     })
     it('event handlers register changes in data they reference', (() => {
-        const data = reactive({ num: 0, clickHandler: function() { this.num++ } })
-        const template = compile(data)`<button @click="clickHandler">Click me: {{ num }}</button>`
+        const { template } = compile({ num: 0, clickHandler: function () { this.num += 1 }})`
+            <button @click="clickHandler">Click me: {{ num }}</button>
+        `
         template.querySelector('button').click()
         expect(template.querySelector('button').textContent).toBe('Click me: 1')
     }))
 })
 describe('compile', () => {
     it('accepts different delimiters', () => {
-        const data = reactive({ string: 'world' })
-        const template = compile(data, ['<%=', '%>'])`<div>Hello <%= string %></div>`
+        const { template } = compile({ string: 'world' }, ['<%=', '%>'])`<div>Hello <%= string %></div>`
         expect(template.textContent).toBe(`Hello world`)
     })
     it('accepts multiple children at root', () => {
-        const data = reactive({ string: 'world' })
-        const template = compile(data)`<div>Hello {{ string }}</div><div>{{ string }} hello!</div>`
+        const { template } = compile({ string: 'world' })`<div>Hello {{ string }}</div><div>{{ string }} hello!</div>`
         expect(template.textContent).toBe(`Hello worldworld hello!`)
     })    
     describe('textContent', () => {
         it('interpolates node content with data', () => {
-            const data = reactive({ string: 'world' })
-            const template = compile(data)`<div>Hello {{ string }}</div>`
+            const { template } = compile({ string: 'world' })`<div>Hello {{ string }}</div>`
             expect(template.textContent).toBe(`Hello world`)
         })
         it('only interpolates content within matching pair of delimiters', () => {
-            const data = reactive({ string: 'world' })
-            const template = compile(data)`<div>Hello <span>{{</span> {{ string }}</div>`
+            const { template } = compile({ string: 'world' })`<div>Hello <span>{{</span> {{ string }}</div>`
             expect(template.textContent).toBe(`Hello {{ world`)
         })
         it('interpolates nested element content with data', () => {
-            const data = reactive({ string: 'world', string2: 'hello' })
-            const template = compile(data)`<div>Hello {{ string }} <p>{{ string2 }} <span>{{ string }}!</span></p></div>`
+            const { template } = compile({ string: 'world', string2: 'hello' })`<div>Hello {{ string }} <p>{{ string2 }} <span>{{ string }}!</span></p></div>`
             expect(template.textContent).toBe(`Hello world hello world!`)
         })
         it('does not alter non-reactive element content', () => {
-            const data = reactive({ string: 'world' })
-            const template = compile(data)`<div>Hello {{ string }} <p>hello!</p></div>`
+            const { template } = compile({ string: 'world' })`<div>Hello {{ string }} <p>hello!</p></div>`
             expect(template.textContent).toBe(`Hello world hello!`)
         })
         it('parses provided javascript', () => {
-            const data = reactive({ num1: 1, num2: 2 })
-            const template = compile(data)`<div>My favorite number is {{ num1 + num2 }}!</div>`
+            const { template } = compile({ num1: 1, num2: 2 })`<div>My favorite number is {{ num1 + num2 }}!</div>`
             expect(template.textContent).toBe(`My favorite number is 3!`)
         })
     })
     describe('attributes', () => {
         it('interpolates node attributes with data', () => {
-            const data = reactive({ data: 'bar' })
-            const template = compile(data)`<div foo="{{ data }}">Hello world!</div>`
+            const { template } = compile({ data: 'bar' })`<div foo="{{ data }}">Hello world!</div>`
             expect(template.querySelector('div').getAttribute('foo')).toBe('bar')
         })
         it('interpolates multiple node attributes with data', () => {
-            const data = reactive({ data: 'bar' })
-            const template = compile(data)`<div foo="{{ data }}" bar="{{ data }}">Hello world!</div>`
+            const { template } = compile({ data: 'bar' })`<div foo="{{ data }}" bar="{{ data }}">Hello world!</div>`
             expect(template.querySelector('div').getAttribute('foo')).toBe('bar')
             expect(template.querySelector('div').getAttribute('bar')).toBe('bar')
         })
         it('does not alter non-reactive node attributes', () => {
-            const data = reactive({ data: 'bar' })
-            const template = compile(data)`<div bar="foo"><p foo="{{ data }}">Hello world!</p></div>`
+            const { template } = compile({ data: 'bar' })`<div bar="foo"><p foo="{{ data }}">Hello world!</p></div>`
             expect(template.querySelector('div').getAttribute('bar')).toBe('foo')
         })
         it('parses provided javascript', () => {
-            const data = reactive({ num1: 1, num2: 2 })
-            const template = compile(data)`<div foo="{{ num1 + num2 }}">Hello world!</div>`
+            const { template } = compile({ num1: 1, num2: 2 })`<div foo="{{ num1 + num2 }}">Hello world!</div>`
             expect(template.querySelector('div').getAttribute('foo')).toBe('3')
         })
         it('adds event listener to element when passed attribute `@[event]`', () => {
-            const data = reactive({ clickHandler: vi.fn()})
-            const template = compile(data)`<button @click="clickHandler">Click me</button>`
+            const { template, data } = compile({ clickHandler: vi.fn()})`<button @click="clickHandler">Click me</button>`
             template.querySelector('button').click()
-            expect(data.clickHandler.value).toHaveBeenCalled()
+            expect(data.clickHandler).toHaveBeenCalled()
         })
     })
     describe('for loop', () => {
         it('compiles all in a loop of elements', () => {
-            const data = reactive({ messages: ['Hello', 'World'] })
-            const template = compile(data)`<div><p &each-messages="message">{{ message }}</p></div>`
-            expect(template.textContent).toBe(`Hello World`)
+            const { template } = compile({ messages: ['Hello', 'World'] })`<div><p &each-messages="message">{{ message }}</p></div>`
+            expect(template.textContent).toBe('HelloWorld')
         })
     })
 })
